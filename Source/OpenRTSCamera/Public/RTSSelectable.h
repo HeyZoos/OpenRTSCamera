2a956b1a8f540eed9b3c97491f11cfcd3763046b
Jesus Bracho <jessebracho@gmail.com>
2024-01-03 20:32:17 -0500

    fix: restore missing RTSSelectable

---
+++ Source/OpenRTSCamera/Public/RTSSelectable.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "RTSSelectable.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class OPENRTSCAMERA_API URTSSelectable : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnSelected();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDeselected();
+};
